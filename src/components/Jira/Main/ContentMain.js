9d1d75735ba1bb1e5afd1d1219e4dea9860b18fe
HoangKhiem3k <ledohoangkhiem3k@gmail.com>
2022-04-06 20:07:03 +0700

    View task

--- src/components/Jira/Main/ContentMain.js
+++ src/components/Jira/Main/ContentMain.js
@@ -6,33 +6,39 @@ export default function ContentMain(props) {
 
     const renderCardTaskList = () => {
         return projectDetail.lstTask?.map((taskListDetail,index) => {
-            return <div key={index} className="card" style={{ width: '17rem', height: '25rem' }}>
+            return <div key={index} className="card pb-2" style={{ width: '17rem', height: 'auto' }}>
             <div className="card-header">
                 {taskListDetail.statusName}
             </div>
             <ul className="list-group list-group-flush">
-                <li className="list-group-item" data-toggle="modal" data-target="#infoModal" style={{ cursor: 'pointer' }}>
-                    <p>
-                        Each issue has a single reporter but can have multiple
-                        assignees
+                {taskListDetail.lstTaskDeTail.map((task,index)=> {
+                    return <li key={index} className="list-group-item"  data-toggle="modal" data-target="#infoModal" style={{ cursor: 'pointer' }}>
+                    <p className="font-weight-300">
+                       {task.taskName}
                     </p>
                     <div className="block" style={{ display: 'flex' }}>
                         <div className="block-left">
-                            <i className="fa fa-bookmark" />
-                            <i className="fa fa-arrow-up" />
+                            <p className="text-danger">{task.priorityTask.priority}</p>
+                            {/* <i className="fa fa-bookmark" />
+                            <i className="fa fa-arrow-up" /> */}
                         </div>
                         <div className="block-right">
                             <div className="avatar-group" style={{ display: 'flex' }}>
-                                <div className="avatar">
-                                    <img src={require("../../../assets/img/download (1).jfif")} alt='1' />
+                                {task.assigness.map((mem,index) => {
+                                    return <div className="avatar" key={index}>
+                                    <img src={mem.avatar} alt={mem.avatar} />
                                 </div>
-                                <div className="avatar">
-                                    <img src={require("../../../assets/img/download (2).jfif")} alt='2' />
-                                </div>
+                                })}
+
+                                
+                               
                             </div>
                         </div>
                     </div>
                 </li>
+                })}
+
+                
               
             </ul>
         </div>
@@ -49,3 +55,34 @@ export default function ContentMain(props) {
 
     )
 }
+
+
+
+{/* <div className="card" style={{ width: '17rem', height: '25rem' }}>
+<div className="card-header">
+    SELECTED FOR DEVELOPMENT 2
+</div>
+<ul className="list-group list-group-flush">
+    <li className="list-group-item">Cras justo odio</li>
+    <li className="list-group-item">Dapibus ac facilisis in</li>
+</ul>
+</div>
+<div className="card" style={{ width: '17rem', height: '25rem' }}>
+<div className="card-header">
+    IN PROGRESS 2
+</div>
+<ul className="list-group list-group-flush">
+    <li className="list-group-item">Cras justo odio</li>
+    <li className="list-group-item">Dapibus ac facilisis in</li>
+</ul>
+</div>
+<div className="card" style={{ width: '17rem', height: '25rem' }}>
+<div className="card-header">
+    DONE 3
+</div>
+<ul className="list-group list-group-flush">
+    <li className="list-group-item">Cras justo odio</li>
+    <li className="list-group-item">Dapibus ac facilisis in</li>
+    <li className="list-group-item">Vestibulum at eros</li>
+</ul>
+</div> */}
